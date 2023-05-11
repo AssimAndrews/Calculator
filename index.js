c7651b9f3a1f2d46c99b378fa9f5e99ac2fdb5f4
EvansOpoku <129169081+EvansOpoku@users.noreply.github.com>
2023-05-11 11:39:58 -1200

    Calculator

---
+++ index.js
@@ -0,0 +1,31 @@
+function add() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 + num2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+function clearResults() {
+    document.getElementById("result").innerHTML = "";
+}
+
+function subtract() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 - num2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+function multiply() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 * num2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
+
+function division() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let result = num1 / num2;
+    document.getElementById("result").innerHTML = "Result: " + result;
+}
